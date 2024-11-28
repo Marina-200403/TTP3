93c1eb091991ace985944146c013d84189a36112
Marina <hyunahanna21@gmail.com>
2024-11-28 22:42:53 +0700

    sort_decr

---
+++ sort_decr.py
@@ -0,0 +1,12 @@
+def sort_decr(mat):
+    size = len(mat)
+    for i in range (size):
+        for j in range(size-i-1):
+            if mat[j]>mat[j+1]:
+                mat[j], mat[j+1] = mat[j+1], mat[j]
+    return mat
+
+if __name__ == "__main__":
+    mat = [4, 6, 2, 1, 8, 3057, 0]
+    print(sort_decr(mat))
+    
